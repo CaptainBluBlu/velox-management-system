9fe0b474227dfc55e525032337d5fb4b7a94d97c
Lim Phang Zhen <limphangzhen@gmail.com>
2023-08-15 22:40:52 +0800

    clearing up file

---
+++ src/components/Customer/create-customer-component.tsx
@@ -0,0 +1,16 @@
+import { Paper } from '@mantine/core'
+import React from 'react'
+
+const createCustomerCompoenent = () => {
+  return (
+    <>
+    <Paper w={100} shadow='xs' radius="md" p="large">
+        
+    </Paper>
+    <div>createCustomerCompoenent</div>
+    </>
+    
+  )
+}
+
+export default createCustomerCompoenent
