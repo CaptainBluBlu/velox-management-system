f96f98dd0398a3e379bea68cfb11e6743bc4077e
Lim Phang Zhen <limphangzhen@gmail.com>
2023-08-17 05:06:17 +0800

    Removed uncessary files and code that are not used (specifically emotion... · 1b0b6b9a)

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -5,27 +5,18 @@ import {
 	ColorScheme,
 	ColorSchemeProvider,
 	MantineProvider,
-	createEmotionCache,
 } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useConfigStore } from '@/stores/config';
-import rtlPlugin from 'stylis-plugin-rtl';
-import RootStyleRegistry from './emotion';
 import { Analytics } from '@/components/Analytics/Analytics';
 
 const queryClient = new QueryClient();
 
-const rtlCache = createEmotionCache({
-	key: 'mantine-rtl',
-	prepend: true,
-	stylisPlugins: [rtlPlugin],
-});
-
 export function AppProvider({ children }: { children: React.ReactNode }) {
-	const { colorScheme, direction, setColorScheme } = useConfigStore();
+	const { colorScheme, setColorScheme } = useConfigStore();
 	const theme = useGlobalTheme({ colorScheme });
 
 	const toggleColorScheme = (value?: ColorScheme) =>
@@ -33,7 +24,7 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<RootStyleRegistry>
+			{/* <RootStyleRegistry> */}
 				<ColorSchemeProvider
 					colorScheme={colorScheme}
 					toggleColorScheme={toggleColorScheme}
@@ -41,15 +32,14 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
 					<MantineProvider
 						withGlobalStyles
 						withNormalizeCSS
-						emotionCache={direction === 'rtl' ? rtlCache : undefined}
-						theme={{ ...theme, dir: direction }}
+						theme={{ ...theme }}
 					>
 						<ModalsProvider>{children}</ModalsProvider>
 						<Notifications />
 						<Analytics />
 					</MantineProvider>
 				</ColorSchemeProvider>
-			</RootStyleRegistry>
+			{/* </RootStyleRegistry> */}
 			{/* <ReactQueryDevtools initialIsOpen={false} /> */}
 		</QueryClientProvider>
 	);
