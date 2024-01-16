65ab2018140d88d2c2552ed8f6db8334ee257d1b
Lim Phang Zhen <limphangzhen@gmail.com>
2024-01-16 11:40:19 +0800

    Test api for datbase connection

---
+++ src/app/api/customers/route.ts
@@ -0,0 +1,12 @@
+import { prisma } from "db";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    await prisma.$connect();
+
+    return new Response("Connected to database", { status: 200 });
+  } catch (error) {
+    return new Response("Error connecting to database", { status: 500 });
+  }
+}
